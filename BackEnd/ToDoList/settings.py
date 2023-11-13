5b98008ca0327c519e08f98141d47a07fea74db4
Alec Smith <alecmsmith18@gmail.com>
2023-11-13 17:15:33 -0600

    changed settings.py

--- BackEnd/ToDoList/settings.py
+++ BackEnd/ToDoList/settings.py
@@ -115,8 +115,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
