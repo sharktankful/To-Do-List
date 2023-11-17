2f7cd5f5effa8f2d45088606922ae658f1b89421
Alec Smith <alecmsmith18@gmail.com>
2023-11-17 13:36:32 -0600

    Setting up Signup Component

--- FrontEnd/ToDo-List/src/App.tsx
+++ FrontEnd/ToDo-List/src/App.tsx
@@ -1,10 +1,16 @@
 import ListGroup from "./components/ListGroup";
 import Form from "./components/Form";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState<string>('');
+
+  const handleTokenChange = (newToken: string) => {
+    setToken(newToken)
+  }
   return (
     <div>
-      <Form/>
+      <Form onTokenChange={handleTokenChange}/>
     </div>
   );
 }
--- FrontEnd/ToDo-List/src/components/Form.tsx
+++ FrontEnd/ToDo-List/src/components/Form.tsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
+import axios from "axios";
 
-const Form = () => {
+interface FormProps {
+  onTokenChange: (token: string) => void;
+}
+
+const Form: React.FC<FormProps> = ({ onTokenChange }) => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const email = emailRef.current?.value || "";
+    const password = passwordRef.current?.value || "";
+
+    try {
+      const response = await axios.post(
+        "http://todolistapi-dev.us-west-2.elasticbeanstalk.com/create/",
+        { email, password }
+      );
+      const token = response.data.token;
+
+      onTokenChange(token);
+    } catch (error) {
+      console.error("Error sending request:", error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">name</label>
-        <input type="name" className="form-control" />
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          ref={emailRef}
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
       </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          ref={passwordRef}
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 };
