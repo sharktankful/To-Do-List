3803530ec0dd0e76511c2407ce340981b9c0e0f1
Alec Smith <alecmsmith18@gmail.com>
2023-11-16 19:53:44 -0600

    Refactored API view endpoints

--- BackEnd/Api/views.py
+++ BackEnd/Api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import firebase, jwt
 
-
+# FIREBASE CONFIGUATION SETTINGS
 config = {
   "apiKey": "AIzaSyBOIInpbo_R5d9CVWnDEv9bd4kAhDnuc9Q",
   "authDomain": "django-todolist-9c4c2.firebaseapp.com",
@@ -15,11 +15,12 @@ config = {
   "measurementId": "G-0ES7CVWSD5"
 }
 
-
+# INITALIZED FIREBASE VARIABLES
 app = firebase.initialize_app(config)
 auth = app.auth()
 database = app.database()
 
+# JWT SECRET KEY
 secret_key = settings.JWT_SECRET_KEY
 
 
@@ -28,9 +29,8 @@ secret_key = settings.JWT_SECRET_KEY
 @api_view(['POST'])
 def create_user(request):
     # EXTRACTS USER SIGNUP INFORMATION FROM FOURM
-    data = request.data
-    email = data.get('email')
-    password = data.get('password')
+    email = request.data.get('email')
+    password = request.data.get('password')
 
     try:
         # CREATES USER AND DATABASE FOR USER
@@ -53,6 +53,7 @@ def create_user(request):
             'token': token
         }
 
+        # RETURNS USER'S JWT TOKEN TO FRONTEND WHEN SIGNED UP
         return Response(response_data, status=201)
     except Exception as e:
         # RETURNS AN ERROR STATUS IF SIGNUP INFO IS INVALID
@@ -63,21 +64,29 @@ def create_user(request):
 @api_view(['POST'])
 def login_user(request):
      # EXTRACTS USER LOGIN INFORMATION FROM FOURM
-    data = request.data
-    email = data.get('email')
-    password = data.get('password')
+    email = request.data.get('email')
+    password = request.data.get('password')
 
     try:
         # LOGIN USER
         user = auth.sign_in_with_email_and_password(email=email, password=password)
-        account_info = auth.get_account_info(user['idToken'])
-        # RETURNS USER'S UID TO FRONTEND WHEN LOGGED IN
-        print("Successfully Logged In!")
-        return Response(account_info, status=201)
+        uid = user['localId']
+
+        # GENERATE A JWT TOKEN
+        token_payload = {'uid': uid}
+        token = jwt.encode(token_payload, secret_key, algorithm='HS256')
+
+        # DATA TO BE INCLUDED IN THE RESPONSE
+        response_data = {
+            'status': 'Successfully logged into account!',
+            'token': token
+        }
+
+        # RETURNS USER'S JWT TOKEN TO FRONTEND WHEN LOGGED IN
+        return Response(response_data, status=201)
     except Exception as e:
         # RETURNS AN ERROR STATUS IF LOG IN INFO IS INVALID
         return Response({'error': str(e)}, status=400)
-
 
 
 
@@ -124,10 +133,11 @@ def get_task(request):
     token = request.headers.get('Authorization', '').split('Bearer ')[1]  # Get the JWT from the Authorization header
 
     try:
-        # Decode the JWT and extract the UID
+        # DECODE THE JWT AND EXTRACT THE UID
         decoded_token = jwt.decode(token, secret_key, algorithms=['HS256'])
         uid = decoded_token.get('uid')
 
+        # EXTRACTS/RETURNS ALL TASKS FROM USER'S DATABASE
         tasks = database.child('Data').child('Users').child(str(uid)).child('Tasks').get().val()
         return Response(tasks, status=200)
     except Exception as e:
