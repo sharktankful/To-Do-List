8bc666387d2c86393d80395ae1cf4759b8ec933d
Alec Smith <alecmsmith18@gmail.com>
2023-11-17 21:21:56 -0600

    Built basic sign up page.

--- FrontEnd/ToDo-List/src/App.tsx
+++ FrontEnd/ToDo-List/src/App.tsx
@@ -1,20 +1,38 @@
 import ListGroup from "./components/ListGroup";
 import Form from "./components/Form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [token, setToken] = useState<string>('');
+  const [token, setToken] = useState<string>("");
+
+  // CHECK LOCAL BROWSER STORAGE FOR JWT TOKEN
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  });
 
   const handleTokenChange = (newToken: string) => {
     setToken(newToken);
+
+    // SAVE TOKEN TO LOCAL BROWSER STORAGE
+    localStorage.setItem("token", newToken);
   };
 
-  
-  return (
-    <div>
-      <Form onTokenChange={handleTokenChange}/>
-    </div>
-  );
+  if (token) {
+    return (
+      <div>
+        <h1>This is the main front page!</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Form onTokenChange={handleTokenChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
